c647a57f447972d0de1f5f347e7df7f4dbc851fd
TaRhondaMayberry <92880394+TaRhondaMayberry@users.noreply.github.com>
2023-03-19 16:10:05 -0700

    Code Files

--- app/src/main/java/com/example/universitywalkingtour/MainActivity.java
+++ app/src/main/java/com/example/universitywalkingtour/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.universitywalkingtour;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        String videoPath = "android.resource://"+ getPackageName() + "/" + R.raw.Video_Instructions;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController( context: this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
 
         Start = (Button)findViewById(R.id.button);
         Start.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/universitywalkingtour/start_page.java
+++ app/src/main/java/com/example/universitywalkingtour/start_page.java
@@ -3,12 +3,45 @@ package com.example.universitywalkingtour;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.view.View;
+import java.util.ArrayList;
 
 public class start_page extends AppCompatActivity {
+    SearchView searchUOPP;
+    ListView listUOPP;
+    ArrayList <String> arrayBuildings;
+    ArrayAdapter <String> adapterBuildings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+        searchUOPP = findViewById(R.id.searchUOP);
+        listUOPP = findViewById(R.id.listUOP);
+        listUOPP.setVisibility(View.GONE);
+        arrayBuildings = new ArrayList <>();
+        arrayBuildings.add("Baun Hall");
+
+        adapterBuildings = new ArrayAdapter <>(this,android.R.layout.simple_list_item_1, arrayBuildings);
+        listUOPP.setAdapter(adapterBuildings);
+        searchUOPP.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                listUOPP.setVisibility(View.VISIBLE);
+                adapterBuildings.getFilter().filter(s);
+
+                return false;
+
+            }
+        });
+
     }
 }
