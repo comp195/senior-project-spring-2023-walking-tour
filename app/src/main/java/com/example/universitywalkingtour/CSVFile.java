0674a2273a28079b891a997ba4bcc0f6d2377041
Alan007-hub <a_khant@u.pacific.edu>
2023-04-09 17:18:25 -0700

    tried to fix the search bar for correct display

--- app/src/main/java/com/example/universitywalkingtour/CSVFile.java
+++ app/src/main/java/com/example/universitywalkingtour/CSVFile.java
@@ -1,10 +1,13 @@
 package com.example.universitywalkingtour;
 
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CSVFile {
@@ -17,13 +20,14 @@ public class CSVFile {
     }
 
     public List<String> read(){
-        List resultList = new ArrayList<>();
+        List<String> resultList = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         try {
             String csvLine;
             while ((csvLine = reader.readLine()) != null) {
                 String[] row = csvLine.split(",");
-                resultList.add(row);
+                Log.i("Row",row[0]);
+                resultList.add(Arrays.asList(row));
             }
         }
         catch (IOException ex) {
