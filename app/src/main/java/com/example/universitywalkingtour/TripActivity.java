48d85afaf261172d78a1f8169206ec981dabdf38
Fangyuan Zhao <milo.zhaoyushan@gmail.com>
2023-05-05 13:55:19 -0700

    Update getCurrentLocation

--- app/src/main/java/com/example/universitywalkingtour/TripActivity.java
+++ app/src/main/java/com/example/universitywalkingtour/TripActivity.java
@@ -4,38 +4,32 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import android.Manifest;
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
-import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
+import android.util.Log;
 import android.widget.Toast;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Objects;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -43,11 +37,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.maps.android.PolyUtil;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -55,19 +45,23 @@ import org.json.JSONObject;
 public class TripActivity extends AppCompatActivity implements OnMapReadyCallback {
     //Lists of buildings
     List<Building> allBuildings;
+    List<Building> wayPoints;
     ArrayList<Building> selectedBuildings;
-    ArrayList<Building> wayPoints;
+
     //Read File
     DirectionSearch directionSearch;
 
     //Map
     private GoogleMap mMap;
-    private LocationRequest locationRequest;
     private double curr_latitude;
     private double curr_longitude;
+    private static final String TAG = "TripActivity";
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
 
     //Selection Window
-    String buildingTypes[] = {"Academic", "Landscape", "Utility", "Dorm", "Office"};
+    String[] buildingTypes = {"Academic", "Landscape", "Utility", "Dorm", "Office"};
     boolean[] selectedItems = {false, false, false, false, false};
 
     @Override
@@ -75,7 +69,7 @@ public class TripActivity extends AppCompatActivity implements OnMapReadyCallbac
         //init
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip);
-        getSupportActionBar().setTitle("UOP Walk");
+        Objects.requireNonNull(getSupportActionBar()).setTitle("UOP Walk");
         selectedBuildings = new ArrayList<>();
 
         //Read File
@@ -85,7 +79,26 @@ public class TripActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         //Map init
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
+
+        //getUserLocation
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    // 更新 curr_longitude 和 curr_latitude 变量
+                    curr_longitude = location.getLongitude();
+                    curr_latitude = location.getLatitude();
+                    Log.d(TAG, "Latitude: " + curr_latitude + ", Longitude: " + curr_longitude);
+                    moveToCurrentLocation();
+                }
+            }
+        };
 
         //Selection Window
         AlertDialog.Builder selectionWindowBuilder = new AlertDialog.Builder(TripActivity.this);
@@ -94,7 +107,7 @@ public class TripActivity extends AppCompatActivity implements OnMapReadyCallbac
         selectionWindowBuilder.setMultiChoiceItems(buildingTypes, selectedItems, new DialogInterface.OnMultiChoiceClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i, boolean isChecked) {
-                 selectedItems[i] = isChecked;
+                selectedItems[i] = isChecked;
             }
         });
         selectionWindowBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
@@ -102,18 +115,16 @@ public class TripActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onClick(DialogInterface dialogInterface, int which) {
                 //Add corresponding buildings to the list
-                for(int i = 0; i < allBuildings.size(); i++){
-                    for(int j = 0; j < selectedItems.length; j++){
-                        if(selectedItems[j] == true && allBuildings.get(i).getType().equals(buildingTypes[j])){
+                for (int i = 0; i < allBuildings.size(); i++) {
+                    for (int j = 0; j < selectedItems.length; j++) {
+                        if (selectedItems[j] && allBuildings.get(i).getType().equals(buildingTypes[j])) {
                             selectedBuildings.add(allBuildings.get(i));
                         }
                     }
                 }
                 setBuildingAudioFileResourceIDs();
-
-                //Ask location permission
-
                 dialogInterface.dismiss();
+                //showPolyLine
                 showPolyLine();
             }
         });
@@ -123,21 +134,31 @@ public class TripActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void showPolyLine() {
+        System.out.println("All selected buildings: ");
+        for(int i = 0; i < selectedBuildings.size(); i++){
+            System.out.println(selectedBuildings.get(i).getName());
+        }
+        System.out.println("origin: " + curr_latitude + "," + curr_longitude);
+
         Building end = selectedBuildings.get(selectedBuildings.size() - 1);
-        List<Building> waypoints = selectedBuildings.subList(0, selectedBuildings.size() - 1);
+
+        wayPoints = selectedBuildings.subList(0, selectedBuildings.size() - 1);
         String apiKey = "AIzaSyCJLJ2SKUEYJg3yjLV2JTM5PbDCX89PUbc";
         String baseUrl = "https://maps.googleapis.com/maps/api/directions/json?";
         String origin = "origin=" + curr_latitude + "," + curr_longitude;
         String destination = "destination=" + end.getLatitude() + "," + end.getLongitude();
         StringBuilder waypointsParam = new StringBuilder("waypoints=");
-        for (int i = 0; i < waypoints.size(); i++) {
-            Building waypoint = waypoints.get(i);
+        System.out.println("Way points: ");
+        for (int i = 0; i < wayPoints.size(); i++) {
+            Building waypoint = wayPoints.get(i);
+            System.out.println(wayPoints.get(i).getName());
             waypointsParam.append(waypoint.getLatitude()).append(",").append(waypoint.getLongitude());
-            if (i < waypoints.size() - 1) {
+            if (i < wayPoints.size() - 1) {
                 waypointsParam.append("|");
             }
         }
-        String url = baseUrl + origin + "&" + destination + "&" + waypointsParam + "&key=" + apiKey;
+        System.out.println("end: " + end.getName());
+        String url = baseUrl + origin + "&" + destination + "&" + waypointsParam + "&key=" + apiKey + "&optimizeWaypoints=true&mode=bicycling";
         System.out.println(url);
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -187,106 +208,69 @@ public class TripActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
-        moveToCurrentLocation();
-        //getCurrentLocation();
-        System.out.println("Current latitude: " + curr_latitude);
-        System.out.println("Current longitude: " + curr_longitude);
+        getCurrentLocation();
+
     }
 
     private void getCurrentLocation() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ActivityCompat.checkSelfPermission(TripActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                if (isGPSEnabled()) {
-                    LocationServices.getFusedLocationProviderClient(TripActivity.this)
-                            .requestLocationUpdates(locationRequest, new LocationCallback() {
-                                @Override
-                                public void onLocationResult(@NonNull LocationResult locationResult) {
-                                    super.onLocationResult(locationResult);
-                                    LocationServices.getFusedLocationProviderClient(TripActivity.this)
-                                            .removeLocationUpdates(this);
-                                    if (locationResult != null && locationResult.getLocations().size() > 0){
-                                        int index = locationResult.getLocations().size() - 1;
-                                        curr_latitude = locationResult.getLocations().get(index).getLatitude();
-                                        curr_longitude = locationResult.getLocations().get(index).getLongitude();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            requestLocationUpdates();
+        }
+    }
 
-                                    }
-                                }
-                            }, Looper.getMainLooper());
+    private void requestLocationUpdates() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+    }
 
-                } else {
-                    turnOnGPS();
-                }
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                requestLocationUpdates();
             } else {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private boolean isGPSEnabled() {
-        LocationManager locationManager = null;
-        boolean isEnabled = false;
-        if (locationManager == null) {
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        }
-        isEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        return isEnabled;
-    }
-
-    private void turnOnGPS() {
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
-        builder.setAlwaysShow(true);
-        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext()).checkLocationSettings(builder.build());
-        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
-            @Override
-            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
-                try {
-                    LocationSettingsResponse response = task.getResult(ApiException.class);
-                    Toast.makeText(TripActivity.this, "GPS is already tured on", Toast.LENGTH_SHORT).show();
-                } catch (ApiException e) {
-                    switch (e.getStatusCode()) {
-                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                            try {
-                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
-                                resolvableApiException.startResolutionForResult(TripActivity.this, 2);
-                            } catch (IntentSender.SendIntentException ex) {
-                                ex.printStackTrace();
-                            }
-                            break;
-                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                            //Device does not have location
-                            break;
-                    }
-                }
-            }
-        });
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        fusedLocationClient.removeLocationUpdates(locationCallback);
     }
 
     private void moveToCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
-            return;
-        }
-        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-                    curr_latitude = location.getLatitude();
-                    curr_longitude = location.getLongitude();
+
+
+                    LatLng currentLatLng = new LatLng(curr_latitude, curr_longitude);
                     CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(currentLatLng, 15); // Change the '15' to your desired zoom level
                     mMap.animateCamera(cameraUpdate);
-                }
-            }
-        });
+
     }
 
+    @SuppressLint("DiscouragedApi")
     private void setBuildingAudioFileResourceIDs() {
         for(int i = 0; i < selectedBuildings.size(); i++){
-            if(selectedBuildings.get(i).getAudioFileName() != "null"){
+            if(!Objects.equals(selectedBuildings.get(i).getAudioFileName(), "null")){
                 selectedBuildings.get(i).setAudioFileResourceID(getResources().getIdentifier(selectedBuildings.get(i).getAudioFileName(), "raw", getPackageName()));
                 System.out.println(selectedBuildings.get(i).getAudioFileResourceID());
             }
