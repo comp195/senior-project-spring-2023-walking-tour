1974fb7c52c9c99ecf14225431ee946a0cd0ef74
tmayberr <t_mayberry@u.pacific.edu>
2023-05-05 20:51:06 -0700

    Audio Files added and new Code

--- app/src/main/java/com/example/universitywalkingtour/MainActivity.java
+++ app/src/main/java/com/example/universitywalkingtour/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.universitywalkingtour;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    boolean TextShow = true;
+    TextView myText =  (TextView) findViewById(R.id.txtView);
     Button startTrip;
     Button startDirecting;
     @Override
@@ -17,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("UOP Walk");
+
+        //media player
+        MediaPlayer mp = new MediaPlayer();
+        String audioPath = "android.resource://" + getPackageName() + "/" + R.raw.instructions;
+        try {
+            mp.setDataSource(this, Uri.parse(audioPath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Log.d("debug","value: " + audioPath);
+
+
 
         //Video
         VideoView videoView = findViewById(R.id.videoView);
@@ -27,8 +47,36 @@ public class MainActivity extends AppCompatActivity {
         mediaController.setAnchorView(videoView);
         videoView.setMediaController(mediaController);
 
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                videoView.start();
+                mp.start();
+            }
+        });
+        mp.prepareAsync();
+
+        videoView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(TextShow == true ){
+                    TextShow = false;
+                    myText.setVisibility(v.INVISIBLE);
+
+                }
+                else{
+                    TextShow = true;
+                    myText.setVisibility(v.VISIBLE);
+                }
+                return false;
+            }
+
+        });
+
         //Trip Mode button
         startTrip = (Button)findViewById(R.id.button_trip);
+
+
         startTrip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
